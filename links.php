bdcc8c5671cc48e946c77857ffcde87790305ef5
Foo Bar <foo@bar.com>
2020-01-31 20:15:10 -0600

    Updated links

--- links.php
+++ links.php
@@ -16,10 +16,10 @@
             #Description --> Link
             $links = [
                 "Leveraging INF-SCT Fetch & Execute Techniques For Bypass, Evasion, & Persistence (bohops)" => "https://bohops.com/2018/02/26/leveraging-inf-sct-fetch-execute-techniques-for-bypass-evasion-persistence/",
-                "A Case Study in Wagging the Dog: Computer Takeover (harmj0y)" => "https://www.harmj0y.net/blog/activedirectory/a-case-study-in-wagging-the-dog-computer-takeover/",
+                "A Case Study in Wagging the Dog: Computer Takeover (@harmj0y)" => "https://www.harmj0y.net/blog/activedirectory/a-case-study-in-wagging-the-dog-computer-takeover/",
                 "The NOP Sled (g00se)" => "http://thenopsled.com/",
-                "Kellgon: The Hacker Tutorial (chrisp)" => "https://kellgon.com/",
-                "Rainier Cyber (bsod_steve)" => "https://www.rainiercyber.com/",
+                "Kellgon: The Hacker Tutorial (@secure_perry)" => "https://kellgon.com/",
+                "Rainier Cyber (bsod_steve @diodepack)" => "https://www.rainiercyber.com/",
             ];
             foreach ($links as $desc => $link) {
                 echo "<li><a href=\"{$link}\">{$desc}</a></li>";
