bb5783db41e29eb61e8dfa033c15a360849df462
Foo Bar <foo@bar.com>
2020-02-22 12:27:21 -0600

    Minor changes

--- disclosures/policy.php
+++ disclosures/policy.php
@@ -4,14 +4,13 @@
     </h3>
     <p class=text>
         Hooper Labs takes security issues seriously.  We believe in working with relevant stakeholders to achieve coordinated disclosure within a reasonable period of time.
-        We also adhere to the industry-standard 90-day disclosure deadline, where vendors are notified of vulnerabilities immediately with details shared with in public after
-        90 days or sooner if the issues are resolved earlier.
+        We also adhere to the industry-standard 90-day disclosure deadline, where vendors are notified of vulnerabilities immediately, with details shared to the public after
+        90 days (or sooner if the issues are resolved earlier).
     </p>
     <p class=text>
         Common Vulnerabilities and Exposures (CVEs) are an industry standard for identifying vulnerabilities <a href="https://en.wikipedia.org/wiki/Common_Vulnerabilities_and_Exposures">
-        (link)</a>.  This system is a method for reference and tracking of publicly-known exposures.  We believe that users have a right to know they are exposed in order to 
-        fully communicate the importance of patching their systems.  MITRE Corporation is the primary CVE Numbering Authority for this program.  A CVE is a way to uniquely reference 
-        vulnerabilities across systems.
+        (link)</a>.  This system is a method for reference and tracking of publicly-known exposures.  A CVE is a way to uniquely reference vulnerabilities across systems and Mitre 
+        Corporation is the primary CVE Numbering Authority (CNA) for the program.  We believe that users have a right to know their exposures in order to make informed risk decisions.   
     </p>
      <p class=text>
         Hooper Labs does not participate in bug bounty programs, but instead relies on responsible disclosure <a href="https://en.wikipedia.org/wiki/Responsible_disclosure">(link)</a>.
