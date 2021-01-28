a45cbd7712d14020df364e9f8be4fc7f98027c54
Cary Hooper <cary.e.hooper.mil@mail.mil>
2021-01-28 13:56:52 -0600

    Updated footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         Contact Us
     </h3>
     <p class="text">
-        If you have any questions, suggestions, or concerns, please reach out on Twitter (@nopantrootdance).  Feel free to contribute to any of the projects on <a href="https://www.github.com/caryhooper">GitHub</a>.
+        If you have any questions, suggestions, or concerns, please reach out on <a href="https://twitter.com/nopantrootdance">Twitter</a> (@nopantrootdance).  Feel free to contribute to any of the projects on <a href="https://www.github.com/caryhooper">GitHub</a>.
     </p>
 </footer>';
 ?>
