9846f207fdd00bcd14667534d75f0fa32e81b262
Cary Hooper <19616873+caryhooper@users.noreply.github.com>
2023-06-07 14:25:35 -0500

    adding randomness

--- footer.php
+++ footer.php
@@ -5,5 +5,12 @@
     <p class="text">
         If you have any questions, suggestions, or concerns, please reach out on <a href="https://twitter.com/nopantrootdance">Twitter</a> (@nopantrootdance).  Feel free to contribute to any of the projects on <a href="https://www.github.com/caryhooper">GitHub</a>.
     </p>
+    </br>
+    </br>
+    <p class="text">';
+echo hash('sha3-512',random_bytes(512));
+?>
+
+<?php echo '</p>
 </footer>';
 ?>
