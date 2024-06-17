a1f4f3c5d6e7cebd648a069f727bd438add026e1
Cary Hooper <19616873+caryhooper@users.noreply.github.com>
2024-06-17 15:03:37 -0500

    Update links.php
    included link to Ben's blog

--- links.php
+++ links.php
@@ -25,7 +25,8 @@
                 
                 "Native VBA Reverse Shell (John Woodman @JohnWoodman15)" => "https://john-woodman.com/research/malicious-vba-macros-trials-tribulation",
                 "Reverse Engineering Course (courtesy of @mytechnotalent)" => "https://github.com/mytechnotalent/Reverse-Engineering",
-                "Exploiting System Mechanic Driver (Paolo Stagno aka VoidSec)" => "https://voidsec.com/exploiting-system-mechanic-driver/"
+                "Exploiting System Mechanic Driver (Paolo Stagno aka VoidSec)" => "https://voidsec.com/exploiting-system-mechanic-driver/",
+                "0xBEN's Blog about Hacking, Powershell, and Coffee" => "https://benheater.com/"
             ];
             foreach ($links as $desc => $link) {
                 echo "<li><a href=\"{$link}\">{$desc}</a></li>";
